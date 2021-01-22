a145143720bb2e18bcd53b3a6308b84991e4ff40
yves <yvesmomplais@hotmail.com>
2021-01-21 21:16:16 -0500

    commit tree dd022d27178449b4d0799798b53f6ef65d538c53

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(value)
+    if value <= 1 || value == 0 || value == 1
+      return false
+    elsif
+      (2..value - 1).each do |i|
+        if value % i == 0
+          return false
+        end
+      end
+    end
+    true
+  end
